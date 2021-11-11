ecbb1b0091afde517766164f10147a1341bf042d
Tom Nash <thomas.c.nash@students.plymouth.ac.uk>
2021-11-11 08:46:09 +0000

    Added New Agent
    - New Agent will take over from old one
    - Will have PHP based upload rather then direct to database
    - Old one will be removed in time

---
+++ assets/AgentFile/NND Agent/NND Agent/NNDAgent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NND_Agent
+{
+    public partial class NNDAgent : Form
+    {
+        public NNDAgent()
+        {
+            InitializeComponent();
+            base.SetVisibleCore(false);
+        }
+        private void NNDAgent_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                NNDToolBarIcon.Visible = true;
+            }
+        }
+
+
+        private void NNDToolBarIcon_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            NNDToolBarIcon.Visible = false;
+
+        }
+
+        private void NNDAgent_Shown(object sender, EventArgs e)
+        {
+            //to minimize window
+            this.WindowState = FormWindowState.Minimized;
+            Hide();
+            NNDToolBarIcon.Visible = true;
+        }
+
+        private void runScanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
