86fa3b805f399c1c28fd8a30a654c4f04abd4a86
Tom Nash <thomas.c.nash@students.plymouth.ac.uk>
2021-11-23 17:42:43 +0000

    Added the ZIP folder creation
    - Added the downloadable (Will change as the app progresses )
    - Made sure a zip is created and is downloadable form end user device

---
+++ assets/AgentFile/DownloadSection/download.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+if(!isset($_SESSION['user_id'])){
+    header('Location: ../../../index.php');
+
+    exit;
+} else {
+
+}
+
+// Get real path for our folder
+$rootPath = realpath('../NNDAgentDownloader');
+
+// Initialize archive object
+$zip = new ZipArchive();
+$fileName = $_SESSION['user_id'] . "Download.zip";
+$zip->open($fileName, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+// Create recursive directory iterator
+//credit to "Niroj" - "https://www.edureka.co/community/82175/how-to-zip-a-whole-folder-using-php";
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+//Credit End
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir())
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+
+// Zip archive will be created only after closing object
+$zip->close();
+
+header('Content-disposition: attachment; filename=files.zip');
+header('Content-type: application/zip');
+
+readfile($fileName);
+
+header('Location: ../../../homePage.php');
+
+//delete the download once downloaded
+unlink($fileName);
+?>
+
+
