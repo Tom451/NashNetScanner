023532d7769b42d6a783e55c992d0a312233eb04
Tom Nash <thomas.c.nash@students.plymouth.ac.uk>
2021-11-25 17:01:08 +0000

    Tided up login form

--- index.php
+++ index.php
@@ -66,19 +66,7 @@ if (isset($_POST['login'])) {
 </head>
 
 <body>
-    <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
-        <div class="container"><a class="navbar-brand" href="#">NashNetworkScanner</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link active" href="#">First Item</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Second Item</a></li>
-                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-toggle="dropdown" href="#">Dropdown </a>
-                        <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
-                    </li>
-                </ul><span class="navbar-text actions"> <a class="login" href="#">Log In</a><a class="btn btn-light action-button" role="button" href="user/registration.php">Sign Up</a></span>
-            </div>
-        </div>
-    </nav>
+    <?php require 'assets/php/navBarLoggedOut.php' ?>
 
     <section class="login-clean" style="width: auto;height: auto;">
         <form method="post">
